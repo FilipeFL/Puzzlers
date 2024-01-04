b3d478be54ac837bba9d19381af16a295eb6d5b3
Filipe Lacerda <43675972+FilipeFL@users.noreply.github.com>
2024-01-04 15:11:11 -0300

    Update Player Movement
    Adding some functions to control friction and acceleration.

--- Puzzlers/Scripts/Player.gd
+++ Puzzlers/Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var max_speed = 300
-@export var accelaration = 1500
+@export var acceleration = 1500
 @export var friction = 1200
 
 @onready var axis = Vector2.ZERO
@@ -43,6 +43,19 @@ func _physics_process(delta):
 func _get_input_axis():
 	axis.x = int(Input.is_action_pressed("Move_Right")) - int(Input.is_action_pressed("Move_Left"))
 	axis.y = int(Input.is_action_pressed("Move_Down")) - int(Input.is_action_pressed("Move_Up"))
+
+
+func _apply_friction(amount):
+	if (velocity.length() > amount):
+		velocity -= velocity.normalized() * amount
+	else:
+		velocity = Vector2.ZERO
+
+
+func _apply_movement(accel): # accel == Acceleration
+	velocity += accel
+	velocity = velocity.limit_length(max_speed)
+
 
 func _player_status():
 	get_parent().get_node("Label").text = get_parent().health_status[health]
