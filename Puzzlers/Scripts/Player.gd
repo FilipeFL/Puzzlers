331b4a408ab3aacdeacfb1d5ff6de880742a273f
Filipe Lacerda <43675972+FilipeFL@users.noreply.github.com>
2024-01-04 15:08:32 -0300

    Player Moving with Axis
    Starting to develop a way for the play to move with the "wasd" keys instead of the mouse.

--- Puzzlers/Scripts/Player.gd
+++ Puzzlers/Scripts/Player.gd
@@ -1,4 +1,10 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var max_speed = 300
+@export var accelaration = 1500
+@export var friction = 1200
+
+@onready var axis = Vector2.ZERO
 
 var type = "PLAYER"
 var health = 5
@@ -11,7 +17,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position = get_global_mouse_position()
+	# Old player movement by the mouse position
+	# self.position = get_global_mouse_position()
+	
+	axis = _get_input_axis()
+	
+	if (axis == Vector2.ZERO):
+		pass # TODO: Apply friction
+	else:
+		pass # TODO: Apply movement
+	
+	move_and_slide()
 	
 	if (Input.is_action_just_pressed("Shoot")):
 		var _bullet = player_bullet.instantiate()
@@ -19,6 +35,14 @@ func _process(delta):
 		get_parent().add_child(_bullet)
 		_bullet.position = self.position
 
+
+func _physics_process(delta):
+	pass
+
+
+func _get_input_axis():
+	axis.x = int(Input.is_action_pressed("Move_Right")) - int(Input.is_action_pressed("Move_Left"))
+	axis.y = int(Input.is_action_pressed("Move_Down")) - int(Input.is_action_pressed("Move_Up"))
 
 func _player_status():
 	get_parent().get_node("Label").text = get_parent().health_status[health]
